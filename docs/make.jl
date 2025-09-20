18818557633ff5a9524be26597de3ca5f54229bd
Andreas Tell <atell@soundtheory.com>
2025-09-20 21:45:24 +0200

    docs: add API Reference page and include internal docs to satisfy Documenter warnings

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,9 @@ Pkg.instantiate()
 
 using Documenter, CliffordAlgebras
 
+# Ensure doctest blocks have access to exported symbols
+DocMeta.setdocmeta!(CliffordAlgebras, :DocTestSetup, :(using CliffordAlgebras); recursive=true)
+
 makedocs(
     sitename = "CliffordAlgebras.jl Documentation",
     modules = [CliffordAlgebras],
@@ -15,6 +18,7 @@ makedocs(
         "Mathematical Background" => "mathematical_background.md",
         "Performance Guide" => "performance.md",
         "Developer Guide" => "developer.md",
+        "API Reference" => "api.md",
     ],
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true"
---
+++ docs/src/api.md
@@ -0,0 +1,19 @@
+# API Reference
+
+This page lists the public API and selected internal functions for advanced users.
+
+## Public API
+
+```@autodocs
+Modules = [CliffordAlgebras]
+Private = false
+Order = [:type, :function]
+```
+
+## Internal API (advanced)
+
+```@autodocs
+Modules = [CliffordAlgebras]
+Private = true
+Order = [:type, :function]
+```
